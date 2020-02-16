c6cf9a854890f5d3747bcee5c5a474821edfd97e
Votizlov <SeriousFox@yandex.ru>
2020-02-16 13:53:47 +0300

    done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-list1 = list(map(int,input('первый список ').split()))
-list2 = list(map(int,input('второй список').split()))
+list1 = list(map(int, input('первый список ').split()))
+list2 = list(map(int, input('второй список').split()))
 answer = []
 
 for i in list1:
-    if i % 2 and not i in list2:
+    if i % 2 == 0 and not i in list2:
         answer.append(i)
 for i in list2:
-    if not i % 2 and not i in list1:
+    if not i % 2 == 0 and not i in list1:
         answer.append(i)
 answer.sort()
 print(answer)
